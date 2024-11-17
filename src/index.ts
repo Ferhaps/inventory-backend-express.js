3c1912ea96b44f212bffdd81d530f22ff0b11367
Ferhan Cherkez <fericherkez14052002@gmail.com>
2024-11-17 19:03:18 +0200

    adds mongo db and user schema

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,4 +20,11 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log('Server started on port 8080');
+});
+
+const MONGO_URL = 'mongodb+srv://FerhanAdmin:Ferhan.jack14502002@inventory-cluster.6v5qq.mongodb.net/?retryWrites=true&w=majority&appName=inventory-cluster';
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => {
+  console.error('db-error', error);
 });
