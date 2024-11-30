2c5d1a0755d296f9eeaf05f6e1baadaf33364512
Ferhan Cherkez <fericherkez14052002@gmail.com>
2024-11-30 15:00:00 +0200

    more user types fixes

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,11 @@
 import mongoose, { Document, Schema } from 'mongoose';
-import { IUser, UserRole } from '../types/user';
+import { UserRole } from '../types/user';
+
+type IUser = {
+  email: string;
+  password: string;
+  role: UserRole;
+}
 
 interface UserDocument extends IUser, Document {}
 
--- src/types/user.ts
+++ src/types/user.ts
@@ -2,9 +2,3 @@ export enum UserRole {
   ADMIN = 'ADMIN',
   OPERATOR = 'OPERATOR'
 }
-
-export type IUser = {
-  email: string;
-  password: string;
-  role: UserRole;
-}
