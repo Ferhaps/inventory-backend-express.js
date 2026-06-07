0dedd51f48d2e4d19fdc3b19cd311a23a4bae039
Ferhan Cherkez <fericherkez14052002@gmail.com>
2026-06-07 16:53:24 +0300

    fixes product dto ts errors

--- src/dto/product.dto.ts
+++ src/dto/product.dto.ts
@@ -1,10 +1,10 @@
 export class ProductDto {
-	id: string;
-	name: string;
-	quantity: number;
-	categoryId: string;
-	createdAt: string;
-	updatedAt: string;
+	id: string = '';
+	name: string = '';
+	quantity: number = 0;
+	categoryId: string = '';
+	createdAt: string = '';
+	updatedAt: string = '';
 
 	constructor(partial: Partial<ProductDto>) {
 		Object.assign(this, partial);
